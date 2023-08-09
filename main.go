b78d4785aaab4cbe6433c594beb65a041c90154e
FelipeRomao <feliperomao.a@gmail.com>
2023-08-08 22:03:22 -0300

    feat: :zap: configure routes with gin-gonic library

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/FelipeRomao/go-opportunities/config"
+import (
+	"github.com/FelipeRomao/go-opportunities/config"
+	"github.com/FelipeRomao/go-opportunities/router"
+)
 
 var (
 	logger *config.Logger
@@ -9,10 +12,14 @@ var (
 func main() {
 	logger = config.GetLogger("main")
 
+	// Initialize the config
 	err := config.Init()
 
 	if err != nil {
 		logger.Errorf("error initializing config: %v", err)
 		return
 	}
+
+	// Initialize the server
+	router.Initialize()
 }
---
+++ router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	router := gin.Default()
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Get the port from the environment variables
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Run the server
+	router.Run(":" + port)
+}
