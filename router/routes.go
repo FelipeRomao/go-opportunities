7e24f0f20ab69909548bbf84bcb0fd8d94bf3d2a
FelipeRomao <feliperomao.a@gmail.com>
2023-08-08 22:23:09 -0300

    feat: :sparkles: implemeting first route for openings

--- router/routes.go
+++ router/routes.go
@@ -1,17 +1,16 @@
 package router
 
 import (
+	"github.com/FelipeRomao/go-opportunities/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/openings", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
-		})
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 
 }
